35be4adf2d2d834751607c1e34563ece353445ab
Kaeh <corentin.grosdemouge@gmail.com>
2023-02-04 13:05:28 +0100

    create rock behavior

--- Scripts/Underground/Grid.gd
+++ Scripts/Underground/Grid.gd
@@ -3,7 +3,7 @@ extends Node2D
 
 func _ready():
 	EVENTS.connect("create_root", self, "_on_create_root")
-
+	_register_rocks()
 
 func _on_create_root(texture: StreamTexture):
 	GAME.set_can_create_root(true)
@@ -14,3 +14,8 @@ func _on_create_root(texture: StreamTexture):
 		add_child_below_node($".", sprite)
 		GAME.set_can_create_root(true)
 	else: GAME.set_can_create_root(false)
+
+func _register_rocks():
+	for rock in $RockGroup.get_children():
+		# Pourquoi il y a 500px qui trainent sur le y ?!
+		GAME._add_to_grid(Vector2(rock.global_position.x, rock.global_position.y - 500))
