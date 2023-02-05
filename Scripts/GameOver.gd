7205648a2b8e085e49dd113bb42ec4f7923ec285
majimo <pierre.fervel@gmail.com>
2023-02-05 12:32:45 +0100

    Add ambiant sounds and game over screen

--- Scripts/GameOver.gd
+++ Scripts/GameOver.gd
@@ -4,3 +4,5 @@ extends Node2D
 func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_accept"):
 		get_tree().change_scene("res://Scenes/World.tscn")
+	
+	$NbrNuits.text = str(GAME.passed_nights) + " nuits"
