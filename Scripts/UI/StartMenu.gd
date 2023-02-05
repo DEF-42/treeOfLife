11685e16c9a23e66224498dc7d55d759ec335785
Kaeh <corentin.grosdemouge@gmail.com>
2023-02-05 16:00:22 +0100

    add menu sound

--- Scripts/UI/StartMenu.gd
+++ Scripts/UI/StartMenu.gd
@@ -2,4 +2,6 @@ extends Node2D
 
 func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_accept"):
+		$ClickSound.play()
+		yield(get_tree().create_timer(0.6), "timeout")
 		get_tree().change_scene("res://Scenes/World.tscn")
